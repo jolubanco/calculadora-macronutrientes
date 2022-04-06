6af8447cf0b019dd86e8c1efe7e0e4d1fb14e281
João Thome <joao.thome@renovretail.com.br>
2022-04-06 11:42:02 -0300

    Corrigindo configurações do swagger

--- src/main/java/com/br/CalculadoraMacroNutrientes/CalculadoraMacroNutrientesApplication.java
+++ src/main/java/com/br/CalculadoraMacroNutrientes/CalculadoraMacroNutrientesApplication.java
@@ -3,7 +3,10 @@ package com.br.CalculadoraMacroNutrientes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CalculadoraMacroNutrientesApplication {
 
 	public static void main(String[] args) {
